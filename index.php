690f329dbee7a97a32a628a97dbeec5d6e941b16
Steve Narancic <58119793+stevenarancic@users.noreply.github.com>
2021-10-08 15:33:06 -0300

    Update index.php

--- index.php
+++ index.php
@@ -123,6 +123,25 @@ $vendedorDAO->readVendedor();
                 } else if (result.isDenied) {}
             })
         }
+
+        function deleteConfirmation() {
+            const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                    toast.addEventListener('mouseenter', Swal.stopTimer)
+                    toast.addEventListener('mouseleave', Swal.resumeTimer)
+                }
+            })
+
+            Toast.fire({
+                icon: 'success',
+                title: 'Vendedor excluído!'
+            })
+        }
     </script>
 </body>
 
